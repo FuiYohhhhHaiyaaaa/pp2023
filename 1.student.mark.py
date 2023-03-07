fc9f56a77f8324e2367445c7a92ce610844a9c10
Phùng Đàm Quân BI12-364 <quanpd.bi12-364@st.usth.edu.vn>
2023-03-08 01:25:22 +0700

    Reup

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,97 +1,80 @@
-STUDENT={} # Dictionary to store info
-def NoOfStudents():     
-    return int(input("Enter number of students: "))  
+def pattern(n):
+    k=2*n-2
+    for i in range(n,-1,-1):
+        for j in range(k,0,-1):
+            print(end=" ")
+        k = k + 1
+        for j in range(0,i + 1):
+            print("*",end=" ")
+        print("\r")
+        
+pattern(5)
 
-a = NoOfStudents()    
-def StudentInfo(a): # Input student info
-    for i in range(1,a+1):
-        studentId = input("Enter student id: ") 
-        studentName = input("Enter student name: ")     
-        studentDOB = input("Enter student's date of birth: ")     
-        return {
-        'id1': studentId,
-        'name': studentName,
-        'dob': studentDOB
-    }
-no_students = NoOfStudents()
-for i in range(no_students):
-    STUDENT += [NoOfStudents()]
+def student_number():
+    number_of_student = int(input("\nEnter No of student in the class: "))
+    return number_of_student
 
-COURSE={} # Dictionary to store info
-def NoOfCourses():     
-    return int(input("Enter number of courses: "))  
+def student_info():
+    id = str(input("\nEnter student id: "))
+    name = str(input("\nEnter student name: "))
+    dob = str(input("\nEnter student date of birth: "))
+    return id, name, dob
 
-b = NoOfCourses()   
-def CourseInfo(b):   # Input course info
-    for i in range(1,b+1):
-        courseID = input("Enter course id: ") 
-        courseName = input ("Enter course name: ")  
-        return{ 
-        'id2': courseID,
-        'name': courseName
-        }
-no_courses = NoOfCourses()
-for i in range(no_courses):
-    COURSE += [NoOfCourses()]
+def student_list():
+    Fuiyohh = []    
+    a = student_number()
+    for i in range(a):
+        id, name, dob = student_info()
+        Fuiyohh.append((id, name, dob))  # add id,name,dob to https://youtube.com/shorts/V4LaRFhkVCA?feature=share
+        print(f"\nWelcome student with student id {id} ")     
+    print("\nStudent list:", Fuiyohh)
+    return Fuiyohh
 
-MARK={} # Dictionary to store info
-def addMark(courseID,studentId):
-    if courseID not in COURSE():
-        print("Invalid course id")
-        return
+def course_number():
+    number_of_course = int(input("\nEnter number of course: "))
+    return number_of_course
+
+def course_info():
+    id = str(input("\nEnter course id: "))
+    name = str(input("\nEnter course name: "))
+    return id, name
+
+def course_list():
+    Fuiyohh = []
+    b = course_number()
+    for i in range(b):
+        id, name = course_info()
+        Fuiyohh.append((id, name))    # add id,name to Fuiyohh list belongin to course_list()
+        print(f"Course {id} aka {name} added")
+    print("\nCourse list", Fuiyohh)
+    return Fuiyohh
+
+def get_mark(): 
+    mark_list = []
+    students_list = []
+    A = course_list()
+    B = student_list()
+    ask_course = input("\nChoose course id: ")
+    Biglist = [students_list,mark_list]
     
-    for studentId in STUDENT():
-        mark = int(input(f"Enter the mark for {STUDENT['id1']['Name']}: "))
-    
-        if studentId not in MARK:
-            MARK[studentId]={}
+    if ask_course in (item for sublist in A for item in sublist):                                 # Using List Comprehension
+        print(f"\nYou choose the {ask_course} course")                                            # lis1 = [1,2]
+                                                                                                  # lis2 = [3,4,5]
+    ask_student = int(input(f"\nHow many student in course {ask_course}?: "))                     # listoflists = [lis1,lis2]
+                                                                                                  # flattenlist = [x  for sublist in listoflists for x in sublist]
+    if ask_student > len(B):                                                                      # print(flattenlist)
+        print(f"\n Invalid!!!")                                                                   # Output: [1,2,3,4,5]
+                                                                                                  # (Variable x is selected from a sublist that is selected from the main list that contains all the other lists)
+    else:                                                                                         
+        for i in range(ask_student):                                                              
+            ask_id = input("\nChoose student id: ")                                               
+            if ask_id in (item for sublist in B for item in sublist):   
+                get_mark = input(f"\nEnter mark for student {ask_id}: ")
+                mark_list.append(get_mark)  #add
+                students_list.append(ask_id)  #add
+                    
+            print("\nIn the course id " + ask_course + ": list of mark is in the format [[student id], [mark]] " )
+            print(Biglist)
 
-        MARK[studentId][courseID] = mark
-
-# Define function to list courses
-def list_courses():
-    for courseID in COURSE:
-        print(f"{courseID}: {COURSE['id2']['name']}")
-
-# Define function to list students
-def list_students():
-    for studentId in COURSE:
-        print(f"{studentId}: {STUDENT['id2']['name']}")
-
-
-# Define function to show marks for a given course
-def show_marks():
-    courseID = input("Enter the course ID: ")
-    if courseID not in COURSE:
-        print("Wrong be@tch!!!")
-        return
-    for id in STUDENT:
-        if id in MARK and courseID in MARK[id]:
-            print(f"{STUDENT[id]['name']}: {MARK[id][courseID]}")
-        else:
-            print(f"{STUDENT[id]['name']}: You die!")
-            break
-
-StudentInfo()
-CourseInfo()
-
-while True:
-    print()
-    print()
-    print()
-    print()
-    print("5.End")
-    choice = input("Please make a choice: ")
-    if choice == "1":
-        addMark()
-    elif choice == "2":
-        list_courses()
-    elif choice == "3":
-        list_students()
-    elif choice == "4":
-        show_marks()
-    elif choice == "5":
-        break
-    else:
-        print("Invalid choice!!!")
-
+get_mark()
+            
